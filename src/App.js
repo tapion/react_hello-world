ab37f798a49cb7be27b99a574d89f365cd0714de
Miguel Angel Vargas <mavargas@HEXACTA.COM>
2020-12-01 14:30:19 -0500

    Se hacen mejoras de rendimiento para React

--- src/App.js
+++ src/App.js
@@ -7,9 +7,11 @@ import UserOutput from './UserOutput/UserOutput';
 function App() {
   const [personsState, setPersonsState] = useState({
     persons: [
-      { name: "Miguel", age: 20},
-      { name: "Angel", age: 30},
-      { name: "Vargas", age: 50},
+      { id: 'asdaq', name: "Miguel", age: 20},
+      { id: 'sad', name: "Angel", age: 30},
+      { id: 'ahggfhsdaq', name: "Vargas", age: 50},
+      { id: 'fdsfs', name: "Cabezas", age: 50},
+      { id: 'dfg', name: "Venegas", age: 50},
     ],
     otherProperty: 'This is a test',
     showPerson: false
@@ -24,25 +26,34 @@ function App() {
     ]
   })
 
-  const changePersonStateHandler = (nameChange) => {
-    //En esta parte estoy editando el estado pero no le mando 'otherProperty'
-    // lo que hace que este atributo se pierda, si lo quiero mantener debo asignarlo de nuevo
-    setPersonsState({persons: [
-        { name: "Miguel - Tapion", age: 20},
-        { name: nameChange, age: 30},
-        { name: "Cabezas", age: 63},
-      ],
-      otherProperty: personsState.otherProperty
+  const changePersonStateHandler = (personId, event) => {
+    const persons = [...personsState.persons];
+    const personIndex = persons.findIndex(per => per.id === personId);
+    persons[personIndex].name = event.target.value;
+    setPersonsState({
+      persons,
+      otherProperty: personsState.otherProperty,
+      showPerson: personsState.showPerson
     });
   }
 
-  const onChangeInputHandler = event => {
-    setPersonsState({persons: [
-      { name: "Miguel - Tapion", age: 20},
-      { name: event.target.value, age: 30},
-      { name: "Cabezas", age: 63},
-    ],
-    otherProperty: personsState.otherProperty
+  // const onChangeInputHandler = event => {
+  //   setPersonsState({persons: [
+  //     { name: "Miguel - Tapion", age: 20},
+  //     { name: event.target.value, age: 30},
+  //     { name: "Cabezas", age: 63},
+  //   ],
+  //   otherProperty: personsState.otherProperty
+  //   });
+  // }
+
+  const deletePersonHandler = indexPerson => {
+    const persons = personsState.persons.slice();
+    persons.splice(indexPerson,1);
+     setPersonsState({
+      persons: persons,
+      otherProperty: personsState.otherProperty,
+      showPerson: personsState.showPerson
     });
   }
 
@@ -77,14 +88,26 @@ function App() {
   if(personsState.showPerson){
     person = (
     <div>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+      { 
+        personsState.persons.map((person, index) => {
+          return (
+            <Person 
+              click={deletePersonHandler.bind(undefined,index)}
+              name={person.name} 
+              age={person.age}
+              change={changePersonStateHandler.bind(undefined,person.id)}
+              key={person.id} />
+          )
+        })
+      }
+      {/* <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
       <Person 
         name={personsState.persons[1].name} 
         age={personsState.persons[1].age} 
         click={changePersonStateHandler.bind(this,'Juan Perez')} 
         change={onChangeInputHandler}
         >I like the dogs</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}  />
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}  /> */}
     </div>
     );
   }
